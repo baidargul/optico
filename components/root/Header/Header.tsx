176bd09ecac92244b50088ff1803cba686467059
Baidar Gul <baidargul@outlook.com>
2024-01-03 23:38:19 +0500

    logo image pointer events none

--- components/root/Header/Header.tsx
+++ components/root/Header/Header.tsx
@@ -8,7 +8,7 @@ const Header = (props: Props) => {
         <div className='bg-gradient-to-b from-zinc-100 to-zinc-300 p-4'>
             <div className='flex items-center'>
                 <div>
-                    <Image src={"/Header/Logo/siteLogo.png"} alt='website-logo' width={80} height={80} draggable={false} />
+                    <Image src={"/Header/Logo/siteLogo.png"} alt='website-logo' width={80} height={80} draggable={false} className='pointer-events-none' />
                 </div>
                 <div className='p-2'>
                     <div className='font-semibold text-2xl text-site-mainText'>
