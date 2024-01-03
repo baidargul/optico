704b9e1b19e917dec286d268385f57d0ef6d22a4
Baidar Gul <baidargul@outlook.com>
2024-01-04 00:56:14 +0500

    profile menu ui

--- components/root/Header/components/MainMenu.tsx
+++ components/root/Header/components/MainMenu.tsx
@@ -7,17 +7,29 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { ArrowDown } from 'lucide-react'
+import Image from 'next/image'
 
 type Props = {}
 
 const MainMenu = (props: Props) => {
     return (
         <DropdownMenu>
-            <DropdownMenuTrigger className='text-site-mainText'>
-                Open
+            <DropdownMenuTrigger className='text-site-mainText transition-all duration-500 focus:outline-none focus:bg-site-mainText/20 rounded p-1'>
+                <div className='flex gap-1 items-center text-site-mainText p-1 group'>
+                    <div>
+                        <Image src={"/Header/Logo/siteLogo.png"} width={30} height={30} alt='profile-picture' className='' />
+                    </div>
+                    <div>
+                        Baidar Gul
+                    </div>
+                    <div>
+                        <ArrowDown className='w-5 h-5 text-site-mainText/60 transition-all duration-1000 opacity-0 group-hover:opacity-100' />
+                    </div>
+                </div>
             </DropdownMenuTrigger>
             <DropdownMenuContent collisionPadding={20}>
-                <DropdownMenuLabel className='select-none text-site-mainText'>My Account</DropdownMenuLabel>
+                <DropdownMenuLabel className='select-none text-site-mainText text-sm'>Super Admin</DropdownMenuLabel>
                 <DropdownMenuSeparator />
                 <DropdownMenuItem>Profile</DropdownMenuItem>
                 <DropdownMenuItem>Billing</DropdownMenuItem>
