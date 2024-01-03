5f2e1c5632da89fc158fa8ad705e2a0c62054c52
Baidar Gul <baidargul@outlook.com>
2024-01-04 01:14:32 +0500

    UI

--- components/root/Header/components/LogoTitle.tsx
+++ components/root/Header/components/LogoTitle.tsx
@@ -5,15 +5,15 @@ type Props = {}
 
 const LogoTitle = (props: Props) => {
     return (
-        <div className='flex items-center'>
-            <div className='rounded-full border-4 border-zinc-500/20 bg-zinc-300'>
+        <div className='flex items-center group cursor-pointer w-fit'>
+            <div className='rounded-full border-4 border-zinc-500/20 bg-zinc-300 transition-all duration-1000 group-hover:border-yellow-600/30 group-hover:bg-yellow-50'>
                 <Image src={"/Header/Logo/siteLogo.png"} alt='website-logo' width={80} height={80} draggable={false} className='pointer-events-none' />
             </div>
-            <div className='p-2'>
+            <div className='p-2 w-fit'>
                 <div className='font-semibold text-2xl text-site-mainText'>
                     Optico
                 </div>
-                <div className='text-sm w-[60%] scale-90 -ml-1 -my-1'>
+                <div className='text-sm w-[60%] scale-90 -ml-1 -my-1 group-hover:tracking-tight transition-all duration-1000'>
                     Retail Solution for eye wear business.
                 </div>
             </div>
