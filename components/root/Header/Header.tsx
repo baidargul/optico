af5d4e2b12b6e637ad65a0ded977247397989027
Baidar Gul <baidargul@outlook.com>
2024-01-03 23:36:28 +0500

    added site logo and styled it

--- components/root/Header/Header.tsx
+++ components/root/Header/Header.tsx
@@ -1,15 +1,26 @@
+import Image from 'next/image'
 import React from 'react'
 
 type Props = {}
 
 const Header = (props: Props) => {
-  return (
-    <div className='bg-gradient-to-b from-zinc-100 to-zinc-300 p-4'>
-        <div className='p-2 font-semibold text-2xl text-site-mainText'>
-            Test Application
+    return (
+        <div className='bg-gradient-to-b from-zinc-100 to-zinc-300 p-4'>
+            <div className='flex items-center'>
+                <div>
+                    <Image src={"/Header/Logo/siteLogo.png"} alt='website-logo' width={80} height={80} draggable={false} />
+                </div>
+                <div className='p-2'>
+                    <div className='font-semibold text-2xl text-site-mainText'>
+                        Optico
+                    </div>
+                    <div className='text-sm w-[60%]'>
+                        Retail Solution for eye wear business.
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Header
