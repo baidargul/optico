151edba9501b92a7d0c5ed5ba71525f6c4639039
Baidar Gul <baidargul@outlook.com>
2024-01-10 20:14:03 +0500

    added properties section to editor

--- components/PropertiesEditor/Editor.tsx
+++ components/PropertiesEditor/Editor.tsx
@@ -1,13 +1,30 @@
+import prisma from "@/lib/prisma"
 import React from 'react'
+import Properties from "./components/Properties"
 
 type Props = {
     id: string //id of Category
 }
 
-const PropertyEditor = (props: Props) => {
+const PropertyEditor = async (props: Props) => {
+    const id = props.id
+
+    const category = await prisma.category.findUnique({
+        include: {
+            nature: true
+        },
+        where: {
+            id: id
+        }
+    })
+    
+    if(!category){
+        return null
+    }
+
     return (
         <div>
-            
+            <Properties category={category}/>
         </div>
     )
 }
---
+++ components/PropertiesEditor/components/Properties.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type Props = {
+    category: any
+}
+
+const Properties = (props: Props) => {
+    return (
+        <div className='w-full bg-gradient-to-r from-amber-50 to-orange-50 h-20 rounded border border-amber-200'>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Properties
