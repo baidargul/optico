a350339b37d059d173a8b04fb6f9fdd547e38c50
Baidar Gul <baidargul@outlook.com>
2024-01-10 20:49:10 +0500

    added close button UI

--- components/PropertiesEditor/components/Properties/Property.tsx
+++ components/PropertiesEditor/components/Properties/Property.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from 'react'
 
 type Props = {
@@ -5,10 +6,15 @@ type Props = {
 }
 
 const Property = (props: Props) => {
+
+    const handleDeletePropertyClick = async () => {
+
+    }
+
     return (
-        <div className='w-full bg-gradient-to-r from-zinc-50 to-zinc-50 h-20 rounded border border-zinc-200/80'>
-            <div>
-                
+        <div className='relative w-full bg-gradient-to-r from-zinc-50 to-zinc-50 h-20 rounded border border-zinc-200/80'>
+            <div onClick={handleDeletePropertyClick} className='absolute right-0 hover:bg-site-colors-secondary bg-site-colors-secondary/40 text-center text-white w-6 h-6 scale-75 text-xs p-1 rounded-md'>
+                x
             </div>
         </div>
     )
