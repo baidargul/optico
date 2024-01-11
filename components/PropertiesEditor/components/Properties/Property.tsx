4d0139438eb9268eeb293b71540805c5d6abb89e
Baidar Gul <baidargul@outlook.com>
2024-01-11 20:43:06 +0500

    UI bug fixed

--- components/PropertiesEditor/components/Properties/Property.tsx
+++ components/PropertiesEditor/components/Properties/Property.tsx
@@ -20,7 +20,7 @@ const Property = (props: Props) => {
     }
 
     return (
-        <div className='relative w-full p-1 text-sm bg-gradient-to-r from-zinc-50 to-zinc-50 h-20 rounded border border-zinc-200/80'>
+        <div className='relative w-full p-1 text-sm bg-gradient-to-r from-zinc-50 to-zinc-50 rounded border border-zinc-200/80'>
             <div onClick={handleDeletePropertyClick} className='absolute right-0 hover:bg-site-colors-secondary bg-site-colors-secondary/40 text-center text-white w-6 h-6 scale-75 text-xs p-1 rounded-md'>
                 x
             </div>
