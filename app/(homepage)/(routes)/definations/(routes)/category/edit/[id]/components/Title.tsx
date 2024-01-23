a49ea37bd583af19bbb1402a6b21f975931fd9ea
Baidar Gul <baidargul@outlook.com>
2024-01-23 19:13:53 +0500

    fixed ui bug

--- app/(homepage)/(routes)/definations/(routes)/category/edit/[id]/components/Title.tsx
+++ app/(homepage)/(routes)/definations/(routes)/category/edit/[id]/components/Title.tsx
@@ -9,6 +9,8 @@ type Props = {
 
 const Title = (props: Props) => {
     const category = props.category
+
+    console.log(category)
     return (
         <div className='flex justify-between items-center'>
             <div className="flex gap-2 items-center group">
@@ -16,7 +18,7 @@ const Title = (props: Props) => {
                 <div>
                     in
                 </div>
-                <div className="p-1 bg-zinc-200 border border-zinc-300 rounded first-letter:font-semibold text-xs">
+                <div className={`p-1 bg-zinc-200 border border-zinc-300 rounded text-xs ${category.nature.dynamic && "first-letter:font-semibold"}`}>
                     {
                         formalizeText(category.nature.name)
                     }
