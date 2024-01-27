1445976466f5fe715a24544e99688cce99203b8b
Baidar Gul <baidargul@outlook.com>
2024-01-27 19:09:03 +0500

    Added New Save Print Buttons

--- app/(homepage)/(routes)/purchase/(route)/invoice/create/page.tsx
+++ app/(homepage)/(routes)/purchase/(route)/invoice/create/page.tsx
@@ -4,6 +4,7 @@ import VendorSelect from './components/vendor/VendorSelect'
 import TermsSelect from './components/TermsSelect'
 import OrderDetails from './components/OrderDetails'
 import Grid from './components/Grid/Grid'
+import { Button } from '@/components/ui/button'
 
 type Props = {}
 
@@ -11,8 +12,15 @@ const page = (props: Props) => {
   return (
     <div className=''>
       <div>
-        <div className='text-2xl text-site-mainText font-semibold mb-5 flex gap-1 items-center'>
-          Purchase Order
+        <div className='text-2xl text-site-mainText font-semibold mb-2 flex gap-1 justify-between items-center'>
+          <div>
+            Purchase Order
+          </div>
+          <div className='p-2 flex gap-2 items-center'>
+            <Button className='bg-site-mainText text-white w-28'>New</Button>
+            <Button className='bg-site-mainText text-white w-28'>Save</Button>
+            <Button className='bg-site-mainText text-white w-28'>Print</Button>
+          </div>
         </div>
         <Separator />
         <div className='py-4 flex justify-between items-start text-sm'>
