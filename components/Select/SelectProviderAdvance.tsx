9e2e19db348dba978984e725812de8a4174b8f46
Baidar Gul <baidargul@outlook.com>
2024-02-19 18:43:52 +0500

    added menu header

--- components/Select/SelectProviderAdvance.tsx
+++ components/Select/SelectProviderAdvance.tsx
@@ -17,7 +17,7 @@ const SelectProviderAdvance = (props: Props) => {
 
     useEffect(() => {
         console.log(item)
-    },[item])
+    }, [item])
 
     return (
         <div onClick={() => setIsToggled(!isToggled)}>
@@ -153,11 +153,29 @@ function ContentP(setIsToggled: any, setItem: any) {
             </div>
             <div className='mt-1 text-sm'>
                 <div>
+                    <div className='grid grid-cols-3 gap-2 truncate text-site-mainText py-1 font-semibold'>
+                        <div>
+                            <div className=''>
+                                Product Name
+                            </div>
+                        </div>
+                        <div>
+                            <div className=''>
+                                Nature
+                            </div>
+                        </div>
+                        <div>
+                            <div className=''>
+                                Category
+                            </div>
+                        </div>
+                    </div>
                     {
                         filteredItems.length > 0 && filteredItems.map((item: any, index: number) => {
 
                             return (
                                 <div className={`${selectedIndex === index && "bg-green-50/80"} text-site-mainText hover:bg-yellow-50/80 w-full text-xs border-b border-dashed`} key={item.id} onClick={() => onItemSelect(item, index)}>
+
                                     <div className='grid grid-cols-3 gap-2 truncate'>
                                         <div>
                                             <div className=''>
@@ -165,7 +183,7 @@ function ContentP(setIsToggled: any, setItem: any) {
                                             </div>
                                         </div>
                                         <div>
-                                            <div className=''>
+                                            <div className={`${item.nature.dynamic && "first-letter:font-semibold"}`}>
                                                 {formalizeText(item.nature.name)}
                                             </div>
                                         </div>
